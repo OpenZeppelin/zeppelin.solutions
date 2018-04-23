8df5db8cd8f51228c163aad7e76c732526c3f21d
Delfina <delfinapolito@gmail.com>
2018-04-23 20:01:12 -0300

    general fixes

--- js/home.js
+++ js/home.js
@@ -6,7 +6,7 @@ $( document ).ready(function(){
       inDuration: 300,
       outDuration: 225,
       constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on hover
+      hover: true, // Activate on hover
       gutter: 0, // Spacing from edge
       belowOrigin: true, // Displays dropdown below the button
       alignment: 'left', // Displays dropdown with edge aligned to the left of button
