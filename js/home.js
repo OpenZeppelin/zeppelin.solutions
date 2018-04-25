4a6b97127274dc47ca9c21ff8feccf71ca2941b7
Delfina <delfinapolito@gmail.com>
2018-04-25 15:42:47 -0300

    fix on audits

--- js/home.js
+++ js/home.js
@@ -9,7 +9,7 @@ $( document ).ready(function(){
       hover: true, // Activate on hover
       gutter: 0, // Spacing from edge
       belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      alignment: 'right', // Displays dropdown with edge aligned to the left of button
       stopPropagation: false // Stops event propagation
     }
   );
