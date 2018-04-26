280d3904ca26f54f55343eb81ef5d0c8b3159663
Delfina <delfinapolito@gmail.com>
2018-04-26 10:52:06 -0300

    audit and jobs process

--- js/home.js
+++ js/home.js
@@ -1,16 +1,18 @@
 $( document ).ready(function(){
-  $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav({
+    edge: 'right',
+  });
   $('.modal').modal();
 
   $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'right', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'right', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
     }
   );
   
