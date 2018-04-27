ccf0b6a54f26de3664fe85644cfb677b365573a4
Delfina <delfinapolito@gmail.com>
2018-04-27 10:29:49 -0300

    dropdown on hover

--- js/home.js
+++ js/home.js
@@ -8,7 +8,7 @@ $( document ).ready(function(){
     inDuration: 300,
     outDuration: 225,
     constrainWidth: false, // Does not change width of dropdown to that of the activator
-    hover: false, // Activate on hover
+    hover: true, // Activate on hover
     gutter: 0, // Spacing from edge
     belowOrigin: true, // Displays dropdown below the button
     alignment: 'right', // Displays dropdown with edge aligned to the left of button
