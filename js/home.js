d548ba5946bcf76367bc6099152f3b89c12370f8
Delfina <delfinapolito@gmail.com>
2018-10-17 18:00:18 +0200

    bounty table

--- js/home.js
+++ js/home.js
@@ -15,7 +15,7 @@ $( document ).ready(function(){
     stopPropagation: false // Stops event propagation
     }
   );
-  
+
   $('.sidenav-close').on( "click", function() {
 	  $('.button-collapse').sideNav('hide');
 	});
