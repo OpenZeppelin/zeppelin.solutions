39520819e74b6ac4929b2624794dbdf2b93b4461
Belén Curcio <curciobelen@gmail.com>
2019-06-27 20:25:53 -0300

    Pixel Perfect

--- js/script.js
+++ js/script.js
@@ -12,11 +12,13 @@
 
   function mobileMenu() {
     return (function() {
+      var $body = $('body');
       var $cont = $('.p-main-nav-mobile');
       var $icon = $('.p-main-nav-mobile .h-icon');
       $icon.click(function handler(evt) {
         $cont.toggleClass('open');
         $icon.toggleClass('open');
+        $body.toggleClass('overlay');
       });
     })();
   }
