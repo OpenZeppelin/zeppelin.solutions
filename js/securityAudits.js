210ab587146899a1dc4c5fa377f374032e6d32a2
Belén Curcio <curciobelen@gmail.com>
2019-07-02 21:41:17 -0300

    validations added

--- js/securityAudits.js
+++ js/securityAudits.js
@@ -97,6 +97,19 @@
         .find('.msr-form-field input')
         .on('blur', blurInput);
 
+      // Catch enter
+
+      $('.msr-form-field input').keydown(function(e) {
+        var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
+        if (key == 13) {
+          e.preventDefault();
+          var inputs = $(this)
+            .closest('form')
+            .find(':input:visible');
+          inputs.eq(inputs.index(this) + 1).focus();
+        }
+      });
+
       $('.sr-form').on('submit', function(e) {
         e.preventDefault();
         validateForm();
