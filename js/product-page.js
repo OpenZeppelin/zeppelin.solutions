16bc9d856ad82ecf65c6ec28e1a6eab7cb699d08
Belén Curcio <curciobelen@gmail.com>
2019-07-08 18:23:25 -0300

    done

--- js/product-page.js
+++ js/product-page.js
@@ -1,14 +1,5 @@
 (function($) {
   $(document).ready(function() {
-    $('.page-product .c-what-else .box').hover(
-      function() {
-        var $prevItem = $(this).prev();
-        $prevItem.addClass('box--moved');
-      },
-      function() {
-        var $prevItem = $(this).prev();
-        $prevItem.removeClass('box--moved');
-      }
-    );
+    //
   });
 })(jQuery);
